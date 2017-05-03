f7e376e51219901a143582561a70e908e4971397
Young J (FCES) <14065258@glam.ac.uk>
2017-05-03 11:39:08 +0100

    Copied over what was in master to this branch and then added the speed up function

--- Source/BeyondTime/BeyondTimeGameMode.cpp
+++ Source/BeyondTime/BeyondTimeGameMode.cpp
@@ -9,7 +9,7 @@ ABeyondTimeGameMode::ABeyondTimeGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonBP/Blueprints/FirstPersonCharacter1"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
